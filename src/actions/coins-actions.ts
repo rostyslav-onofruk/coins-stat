edd6a3939e6e933126eba4fd82e30b2e5bb32eb8
Rostyslav Onofruk <rostyslav.onofruk@sombrainc.com>
2020-01-10 19:11:33 +0200

    added base and coinsDetails data to redux

--- src/actions/coins-actions.ts
+++ src/actions/coins-actions.ts
@@ -1,8 +1,10 @@
 import client from '../services/client';
-import {Dispatch} from "react";
+import {Dispatch} from 'react';
 
 export const COINS_FETCH_SUCCESS = 'COINS_FETCH_SUCCESS';
 export const COINS_FETCH_FAILURE = 'COINS_FETCH_FAILURE';
+export const COIN_HISTORY_FETCH_SUCCESS = 'COIN_HISTORY_FETCH_SUCCESS';
+export const COIN_HISTORY_FETCH_FAILURE = 'COIN_HISTORY_FETCH_FAILURE';
 
 interface Arg {
     type: string;
@@ -21,9 +23,26 @@ export const fetchAllCoins = () => (dispatch: Dispatch<Arg>) => {
         .catch(err => {
             dispatch({
                 type: COINS_FETCH_FAILURE,
-                payload: err.message
+                payload: {...err}
             });
         });
 };
 
-
+export const fetchCoin = (id: number) => (dispatch: Dispatch<Arg>) => {
+    client()
+        .get(`/coin/${id}/history/24h`)
+        .then(res => {
+            console.log('WORKS');
+            dispatch({
+                type: COIN_HISTORY_FETCH_SUCCESS,
+                payload: {...res.data, id}
+            });
+        })
+        .catch(err => {
+            console.log('FAIL');
+            dispatch({
+                type: COIN_HISTORY_FETCH_FAILURE,
+                payload: {...err}
+            });
+        });
+};
--- src/reducers/coins-reducer.ts
+++ src/reducers/coins-reducer.ts
@@ -1,15 +1,30 @@
-import {COINS_FETCH_FAILURE, COINS_FETCH_SUCCESS} from '../actions/coins-actions';
-import {Coin} from "../interfaces/coins";
+import {
+    COIN_HISTORY_FETCH_FAILURE,
+    COIN_HISTORY_FETCH_SUCCESS,
+    COINS_FETCH_FAILURE,
+    COINS_FETCH_SUCCESS
+} from '../actions/coins-actions';
+import {FullCoin, History, ShortCoin} from '../interfaces/coins';
+import {Base} from '../interfaces/base';
 
 const INITIAL_STATE: CoinsReducer = {
-    data: {},
+    data: {
+        base: {
+            symbol: '',
+            sign: '',
+        },
+        coins: [],
+        coinsDetails: []
+    },
     loaded: null,
     error: null,
 };
 
 interface CoinsReducer {
     data: {
-        coins?: Coin[];
+        base: Base;
+        coins: ShortCoin[];
+        coinsDetails: FullCoin[]
     },
     loaded: boolean | null;
     error: boolean | null;
@@ -19,8 +34,11 @@ interface Action {
     type: string;
     payload: {
         data: {
-            coins: Coin[];
+            base: Base;
+            coins: ShortCoin[];
+            history: History[];
         };
+        id: number;
     }
 }
 
@@ -28,11 +46,36 @@ export default function coinsReducer(state = INITIAL_STATE, action: Action) {
     const {type, payload} = action;
     switch (type) {
         case COINS_FETCH_SUCCESS: {
-            return {data: {...payload.data}, loaded: true, error: false};
+            return {
+                data: {...state.data,
+                    coins: [...payload.data.coins.map(({id, name, iconUrl, color, websiteUrl, rank, description}) => {
+                        return {id, name, iconUrl, color, websiteUrl, rank, description}
+                    })],
+                    base: {...payload.data.base}},
+                loaded: true, error: false
+            };
         }
         case COINS_FETCH_FAILURE:
             console.error('COINS_FETCH_FAILURE', payload);
-            return {data: {}, loaded: false, error: true};
+            return {data: {...state.data}, loaded: false, error: true};
+        case COIN_HISTORY_FETCH_SUCCESS: {
+
+            const newCoinsDetails = [...state.data.coinsDetails];
+            const index = state.data.coins.map(coin => coin.id).indexOf(payload.id);
+            const existedIndex = state.data.coinsDetails.map(coin => coin.id).indexOf(payload.id);
+            const newCoinInfo = {...state.data.coins[index], history: [...payload.data.history]};
+
+            if (existedIndex === -1) {
+                newCoinsDetails.push(newCoinInfo);
+            } else {
+                newCoinsDetails[existedIndex] = newCoinInfo;
+            }
+
+            return {data: {...state.data, coinsDetails: newCoinsDetails}, loaded: false, error: true};
+        }
+        case COIN_HISTORY_FETCH_FAILURE:
+            console.error('COIN_HISTORY_FETCH_FAILURE', payload);
+            return {data: {...state.data}, loaded: false, error: true};
         default:
             return state;
     }
