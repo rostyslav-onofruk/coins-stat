965a4aec47403eef81421d770d63794d4152bebc
Rostyslav Onofruk <rostyslav.onofruk@sombrainc.com>
2020-01-13 19:13:42 +0200

    added redux-persist for saving store in localStorage

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
-import {Store} from 'redux';
 import ReactDOM from 'react-dom';
+import {applyMiddleware, createStore, Store} from 'redux'
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistReducer, persistStore} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import {Provider} from 'react-redux';
-import './index.css';
 import Routes from './routes';
 import * as serviceWorker from './serviceWorker';
-import configureStore from './store';
 
-// @ts-ignore
-export const store: Store = configureStore();
+import rootReducer from './reducers';
+import thunk from "redux-thunk";
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store: Store = createStore(persistedReducer, applyMiddleware(thunk));
+const persistor = persistStore(store);
+export default {store, persistor};
 
 ReactDOM.render(
     <Provider store={store}>
-        <Routes/>
+        <PersistGate loading={null} persistor={persistor}>
+            <Routes/>
+        </PersistGate>
     </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
